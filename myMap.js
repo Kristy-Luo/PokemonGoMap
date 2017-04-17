9761288c46ee99d9b3032bb14663596a7d5aa267
Kristy Luo <Kristy-Luo@users.noreply.github.com>
2017-04-17 11:24:33 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,8 +1,7 @@
 // Bing Map V8 Web Control API: load map (async)
-
 var map;
 function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'Your Bing Maps Key'
+        credentials: 'AorhxR-W15VbulU0aX5b-KPHXEEnhPtvtzHMrGNTwkYdhUeQ2Fzky8dzELJrPezY'
     });
 }
